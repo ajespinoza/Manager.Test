4e00901d084f720f1041888f76abc900d9ff7df3
ajespinoza <angelo_19_94@hotmail.com>
2023-06-26 15:26:19 -0600

    Prueba de remote server

--- Manager.Client/Program.cs
+++ Manager.Client/Program.cs
@@ -18,7 +18,7 @@ namespace Manager.Client
         static BinaryClientFormatterSinkProvider clientProv;
         static BinaryServerFormatterSinkProvider serverProv;
 
-        private static string serverUri = "tcp://192.168.1.68:9096/ManagerAccount";
+        private static string serverUri = "tcp://192.168.1.194:9096/ManagerAccount";
         private static bool connected = false;
 
         public static void Main(string[] args)
@@ -26,81 +26,81 @@ namespace Manager.Client
             #region ManagerSencillo
             //Cliente para manager sencillo
             //Sin eventos 
-            TcpChannel channel = new TcpChannel();
-            ChannelServices.RegisterChannel(channel, ensureSecurity: false);
-            AccountMove accountMove = (AccountMove)Activator.GetObject(typeof(Manager.Core.AccountMove),
-                                        "tcp://localhost:9096/ManagerAccount");
+            //TcpChannel channel = new TcpChannel();
+            //ChannelServices.RegisterChannel(channel, ensureSecurity: false);
+            //AccountMove accountMove = (AccountMove)Activator.GetObject(typeof(Manager.Core.AccountMove),
+            //                            "tcp://localhost:9096/ManagerAccount");
 
 
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Client connected!!");
+            //Console.ForegroundColor = ConsoleColor.Green;
+            //Console.WriteLine("Client connected!!");
 
-            decimal move = 10;
-            Console.WriteLine($"Debit move for {move}");
-            accountMove.Debit(move);
+            //decimal move = 10;
+            //Console.WriteLine($"Debit move for {move}");
+            //accountMove.Debit(move);
 
-            Thread.Sleep(TimeSpan.FromSeconds(10));
-            move = 12;
-            Console.WriteLine($"Debit move for {move}");
-            accountMove.Debit(move);
+            //Thread.Sleep(TimeSpan.FromSeconds(10));
+            //move = 12;
+            //Console.WriteLine($"Debit move for {move}");
+            //accountMove.Debit(move);
 
-            Thread.Sleep(TimeSpan.FromSeconds(10));
-            move = 15;
-            Console.WriteLine($"Debit move for {move}");
-            accountMove.Debit(move);
+            //Thread.Sleep(TimeSpan.FromSeconds(10));
+            //move = 15;
+            //Console.WriteLine($"Debit move for {move}");
+            //accountMove.Debit(move);
 
 
-            Console.WriteLine($"El saldo total es {accountMove.GetAmount()}");
-            Console.ReadLine();
+            //Console.WriteLine($"El saldo total es {accountMove.GetAmount()}");
+            //Console.ReadLine();
             #endregion
 
 
 
             #region ManagerMasComplejo
             ////Manager mas completo
-            //clientProv = new BinaryClientFormatterSinkProvider();
-            //serverProv = new BinaryServerFormatterSinkProvider();
-            //serverProv.TypeFilterLevel = System.Runtime.Serialization.Formatters.TypeFilterLevel.Full;
+            clientProv = new BinaryClientFormatterSinkProvider();
+            serverProv = new BinaryServerFormatterSinkProvider();
+            serverProv.TypeFilterLevel = System.Runtime.Serialization.Formatters.TypeFilterLevel.Full;
 
-            ////Manejo una clase que encapsula un evento para poder integrar logs de disparo o error
-            //changeAmount = new RemoteEvent<AccountArgs>();
-            //changeAmount.eventToHandle += new EventHandler<AccountArgs>(changeAmountEvent);
+            //Manejo una clase que encapsula un evento para poder integrar logs de disparo o error
+            changeAmount = new RemoteEvent<AccountArgs>();
+            changeAmount.eventToHandle += new EventHandler<AccountArgs>(changeAmountEvent);
 
-            //Hashtable props = new Hashtable();
-            //props["name"] = "remotingClient";
-            //props["port"] = 0;
+            Hashtable props = new Hashtable();
+            props["name"] = "remotingClient";
+            props["port"] = 0;
 
-            //channel = new TcpChannel(props, clientProv, serverProv);
-            //ChannelServices.RegisterChannel(channel);
+            channel = new TcpChannel(props, clientProv, serverProv);
+            ChannelServices.RegisterChannel(channel);
 
-            //RemotingConfiguration.RegisterWellKnownClientType(new WellKnownClientTypeEntry(typeof(IAccountMove), serverUri));
+            RemotingConfiguration.RegisterWellKnownClientType(new WellKnownClientTypeEntry(typeof(IAccountMove), serverUri));
 
-            //if (connected)
-            //    return;
+            if (connected)
+                return;
 
-            //try
-            //{
+            try
+            {
 
-            //    accountMove = (IAccountMove)Activator.GetObject(typeof(IAccountMove), serverUri);
-            //    //Para encapsular el evento en una clase para manejar logs
-            //    accountMove.changeAmount += new EventHandler<AccountArgs>(changeAmount.Notify);
-            //    //accountMove.changeAmount += changeAmountEvent;
+                accountMove = (IAccountMove)Activator.GetObject(typeof(IAccountMove), serverUri);
+                //Para encapsular el evento en una clase para manejar logs
+                accountMove.changeAmount += new EventHandler<AccountArgs>(changeAmount.Notify);
+                //accountMove.changeAmount += changeAmountEvent;
 
-            //    accountMove.Debit(12);
-            //    accountMove.Debit(30);
-            //    accountMove.Debit(50);
-            //    connected = true;
+                accountMove.Debit(12);
+                accountMove.Debit(30);
+                accountMove.Debit(50);
+                connected = true;
 
-            //}
-            //catch (Exception ex)
-            //{
-            //    connected = false;
-            //    Console.ForegroundColor = ConsoleColor.Red;
-            //    Console.WriteLine($"Could not connect {ex.Message}");
-            //}
+            }
+            catch (Exception ex)
+            {
+                connected = false;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Could not connect {ex.Message}");
+            }
 
-            //Console.ReadLine();
-            //Disconnect();
+            Console.ReadLine();
+            Disconnect();
             #endregion
 
         }
--- Manager.Host/Program.cs
+++ Manager.Host/Program.cs
@@ -12,37 +12,37 @@ namespace Manager.Host
         public static void Main(string[] args)
         {
             #region ManagerSencillo
-            //Manager Version basica
-            //Sin eventos
-            Hashtable props = new Hashtable();
-            props["port"] = 9096;
-            props["name"] = "ManagerAccount";
+            ////Manager Version basica
+            ////Sin eventos
+            //Hashtable props = new Hashtable();
+            //props["port"] = 9096;
+            //props["name"] = "ManagerAccount";
 
-            BinaryServerFormatterSinkProvider serverProv = new BinaryServerFormatterSinkProvider();
-            serverProv.TypeFilterLevel = System.Runtime.Serialization.Formatters.TypeFilterLevel.Full;
+            //BinaryServerFormatterSinkProvider serverProv = new BinaryServerFormatterSinkProvider();
+            //serverProv.TypeFilterLevel = System.Runtime.Serialization.Formatters.TypeFilterLevel.Full;
 
-            TcpServerChannel channel = new TcpServerChannel(props, serverProv);
-            ChannelServices.RegisterChannel(channel, ensureSecurity: false);
+            //TcpServerChannel channel = new TcpServerChannel(props, serverProv);
+            //ChannelServices.RegisterChannel(channel, ensureSecurity: false);
 
-            RemotingConfiguration.RegisterWellKnownServiceType(
-                System.Type.GetType("Manager.Core.AccountMove, Manager.Core"),
-                "ManagerAccount",
-                WellKnownObjectMode.Singleton);
+            //RemotingConfiguration.RegisterWellKnownServiceType(
+            //    System.Type.GetType("Manager.Core.AccountMove, Manager.Core"),
+            //    "ManagerAccount",
+            //    WellKnownObjectMode.Singleton);
 
-            System.Console.WriteLine("Manager running. Press <enter> to exit...");
-            System.Console.ReadLine();
+            //System.Console.WriteLine("Manager running. Press <enter> to exit...");
+            //System.Console.ReadLine();
             #endregion
 
 
             #region ManagerComplejo
             //Levanto manager mas completo
-            //AccountMove accountServer = new AccountMove();
-            //Console.ForegroundColor = ConsoleColor.Green;
-            //Console.WriteLine("Manager initialize...");
-            //accountServer.Initialize();
-            //Console.WriteLine("Press any key for stop..");
-            //Console.ReadLine();
-            //accountServer.Uninitialize();
+            AccountMove accountServer = new AccountMove();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Manager initialize...");
+            accountServer.Initialize();
+            Console.WriteLine("Press any key for stop..");
+            Console.ReadLine();
+            accountServer.Uninitialize();
             #endregion
 
         }
